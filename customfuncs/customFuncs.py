e0847b6daabe7bd02456c96d60b7ac225a04b187
Joe Fahnestock <joeyfahnestock@gmail.com>
2023-09-03 15:27:11 -0400

    Ported datetime.go and customfuncs.go but still need to finish resolving flake8 lint

--- customfuncs/customFuncs.py
+++ customfuncs/customFuncs.py
@@ -30,32 +30,28 @@ def _ConvertTZ(t: time.struct_time, tz: str) -> time.struct_time:
     dt_utc = datetime(*t[:6], tzinfo=timezone.utc)
     dt_tz = dt_utc.astimezone(fromTZ)
     return time.struct_time(
-        tuple(
-            a
-            for a in [
-                *dt_tz.timetuple(),
-                ZoneInfo(tz),
-                dt_tz.utcoffset().total_seconds(),
-            ]
-        )
+        (*dt_tz.timetuple(), ZoneInfo(tz), dt_tz.utcoffset().total_seconds())
     )
 
+
 def _OverwriteTZ(t: time.struct_time, tz: str) -> time.struct_time:
-    return time.struct_time(tuple(*t, ZoneInfo(tz), ZoneInfo(tz).utcoffset(datetime(*t[:6])).total_seconds()))
+    return time.struct_time(
+        (*t, ZoneInfo(tz), ZoneInfo(tz).utcoffset(datetime(*t[:6])).total_seconds())
+    )
 
 
 def parseDateTime(
-    datetime: str, layout: str, layoutHasTZ: bool, fromTZ: str, toTZ: str
+    datetime_in: str, layout: str, layoutHasTZ: bool, fromTZ: str, toTZ: str
 ) -> Tuple[time.struct_time, bool, Exception]:
     if layout == "":
         try:
-            t: time.struct_time = time.strptime(datetime)
+            t: time.struct_time = time.strptime(datetime_in)
             hasTZ = bool(t.tm_zone)
         except Exception as err:
             return go_time_zero, False, err
     else:
         try:
-            t: time.struct_time = time.strptime(datetime, layout)
+            t: time.struct_time = time.strptime(datetime_in, layout)
         except Exception as err:
             return go_time_zero, False, err
         hasTZ = layoutHasTZ
@@ -79,6 +75,121 @@ def parseDateTime(
     return t, hasTZ, None
 
 
+def rfc3339(t: time.struct_time, hasTZ: bool) -> str:
+    if hasTZ:
+        return datetime(*t[:6], tzinfo=t.tm_zone).isoformat()
+    return datetime(*t[:6]).isoformat()
+
+
+def DateTimeToRFC3339(
+    datetime_in: str, fromTZ: str, toTZ: str, _: transformctx.Ctx = None
+) -> Tuple[str, Exception]:
+    if datetime_in == "":
+        return "", None
+    t, hasTZ, err = parseDateTime(datetime_in, "", False, fromTZ, toTZ)
+    if err != None:
+        return "", err
+    return rfc3339(t, hasTZ), None
+
+
+def _ParseBool(s: str) -> bool:
+    truthy_vals = ["1", "t", "T", "TRUE", "true", "True"]
+    falsy_vals = ["0", "f", "F", "FALSE", "false", "False"]
+    if s in trutyh_vals:
+        return True
+    elif s in falsy_vals:
+        return False
+    else:
+        raise Exception(
+            "Boolean value not parseable. See https://pkg.go.dev/strconv#ParseBool for acceptable values."
+        )
+
+
+def DateTimeLayoutToRFC3339(
+    datetime_in: str,
+    layout: str,
+    layoutTZ: str,
+    fromTZ: str,
+    toTZ: str,
+    _: transform.Ctx = None,
+) -> Tuple[str, Exception]:
+    layoutTZFlag = False
+    if (layout != "") and (layoutTZ != ""):
+        try:
+            layoutTZFlag = _ParseBool(layoutTZ)
+        except Exception as err:
+            return "", err
+    if datetime_in == "":
+        return "", None
+    t, hasTZ, err = parseDateTime(datetime_in, layout, layoutTZFlag, fromTZ, toTZ)
+    if err != None:
+        return "", None
+    return rfc3339(t, hasTZ), None
+
+
+epochUnitMilliseconds = "MILLISECOND"
+epochUnitSeconds = "SECOND"
+
+
+def DateTimeToEpoch(
+    datetime_in: str, fromTZ: str, unit: str, _: transformctx.Ctx = None
+) -> Tuple[str, Exception]:
+    # NOTE: May have issues with python ints as source code uses 64 bit ints.
+    if datetime_in == "":
+        return "", None
+    t, _, err = parseDateTime(datetime_in, "", False, fromTZ, "")
+    if err != None:
+        return "", err
+    if unit == epochUnitMilliseconds:
+        return int(datetime(*t[:6], tzinfo=t.tm_zone).timestamp() * 1000), None
+    elif epochUnitSeconds:
+        return int(datetime(*t[:6], tzinfo=t.tm_zone).timestamp()), None
+    else:
+        return "", Exception(f"Unknown epoch unit {unit}.")
+
+
+class _epoch_to_date_time_rfc3339_sentinel(str):
+    pass
+
+
+def EpochToDateTimeRFC3339(
+    epoch: str,
+    unit: str,
+    tz: str = _epoch_to_date_time_rfc3339_sentinel(),
+    _: transformctx.Ctx = None,
+) -> Tuple[str, Exception]:
+    if epoch == "":
+        return "", None
+    try:
+        n = int(epoch)
+    except Exception as err:
+        return "", err
+    tz_used = "UTC"
+    if not isinstance(tz, _epoch_to_date_time_rfc3339_sentinel):
+        tz_used = tz
+    try:
+        loc = ZoneInfo(tz_used)
+    except Exception as err:
+        return "", err
+    if unit == epochUnitSeconds:
+        dt = datetime.fromtimestamp(n).astimezone(loc)
+        t = time.struct_time(
+            (*dt.timetuple(), ZoneInfo(tz_used), dt.utcoffset().total_seconds())
+        )
+    elif unit == epochUnitMilliseconds:
+        dt = datetime.fromtimestamp(n).astimezone(loc)
+        t = time.struct_time(
+            (*dt.timetuple(), ZoneInfo(tz_used), dt.utcoffset().total_seconds())
+        )
+    else:
+        return "", Exception(f"Unknown epoch unit {unit}")
+    return rfc3339(t, True), None
+
+
+def Now(_: transformctx.Ctx = None) -> Tuple[str, Exception]:
+    return rfc3339(time.struct_time((*time.gmtime(), ZoneInfo("UTC")), True)), None
+
+
 ########################
 # Port of customFuncs.go
 ########################
@@ -92,27 +203,27 @@ def Merge(*funcs) -> CustomFuncs:
     return merged
 
 
-def Coalesce(_: transformctx.Ctx = None, *strs) -> Tuple[string, Exception]:
+def Coalesce(*strs, _: transformctx.Ctx = None) -> Tuple[string, Exception]:
     for _, s in strs:
         if s != "":
             return s, None
     return "", None
 
 
-def Concat(_: transformctx.Ctx = None, *strs) -> Tuple[str, Exception]:
+def Concat(*strs, _: transformctx.Ctx = None) -> Tuple[str, Exception]:
     result = "".join(strs)
     return result, None
 
 
-def Lower(_: transform.Ctx, s: str = "") -> Tuple[str, Exception]:
+def Lower(s: str = "", _: transform.Ctx = None) -> Tuple[str, Exception]:
     return s.lower(), None
 
 
-def Upper(_: transformctx.Ctx = None, s: str = "") -> Tuple[str, Exception]:
+def Upper(s: str = "", _: transformctx.Ctx = None) -> Tuple[str, Exception]:
     return s.upper(), None
 
 
-def UUIDv3(_: transformctx.Ctx = None, s: str = "") -> Tuple[str, Exception]:
+def UUIDv3(s: str = "", _: transformctx.Ctx = None) -> Tuple[str, Exception]:
     return str(uuid.uuid3(uuid.UUID(int=0), s)), None
 
 
